6e6442531c4123110cd1acf2841a0fe44673ad9f
xialvjun <xialvjun@live.com>
2023-05-29 20:25:13 +0800

    fix h

--- src/h.ts
+++ src/h.ts
@@ -2,7 +2,16 @@ const EMPTY_OBJECT = {};
 
 export function h(type: any, props: any, ...children: any[]) {
   props = props ?? EMPTY_OBJECT;
-  props = children.length ? Object.assign({}, props, { children }) : props;
+
+  // jsx/jsxDEV 处理 children 都是 children.length===1 时特殊处理，无法变更
+  // 为了让 h 的效果与 jsx 相同，也单独处理下它
+  props =
+    children.length > 1
+      ? Object.assign({}, props, { children })
+      : children.length === 1
+      ? Object.assign({}, props, { children: children[0] })
+      : props;
+
   return jsx(type, props, props.key);
 }
 
